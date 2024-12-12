88cae603aeefee6c05ccfdabe2e28c77b06f0177
Ironboy <ironboy@users.noreply.github.com>
2024-12-12 11:44:39 +0100

    working store

--- main.js
+++ main.js
@@ -1,3 +1,17 @@
+// store - simplifies the usage of localStorage
+let store;
+try {
+  store = JSON.parse(localStorage.store);
+}
+catch (e) {
+  store = {};
+}
+
+store.save = function () {
+  localStorage.store = JSON.stringify(this);
+};
+
+
 let airports = await $.ajax('airports.json');
 
 // filter and sort airports (only North American Airports, ordered by name)
@@ -5,4 +19,54 @@ airports = airports
   .filter(airport => airport.continent === 'NA')
   .sort((a, b) => a.name > b.name ? 1 : -1);
 
-console.log(airports);
+// create an array with the codes of the users favorite airports
+// in the store, if it doesn't exist already
+if (!store.favorites) {
+  store.favorites = [];
+  store.save();
+}
+
+function showAirports() {
+  // empty the contents of body
+  $('body').empty();
+  // lop through the airports and add html to the body
+  for (let { code, name, location, elevationFeet, continent, countryCode } of airports) {
+    let favoriteClass = '';
+    if (store.favorites.includes(code)) {
+      favoriteClass = 'favorite'
+    }
+    $('body').append(`
+      <article class="airport ${favoriteClass}">
+        <h3>${name}</h3>
+        <p>Code: <span class="code">${code}</span></p>
+        <p>Location: ${location}</p>
+        <p>Elevation (m) ${(elevationFeet * 0.3048).toFixed(2)}</p>
+        <p>Continent: ${continent}</p>
+        <p>Country code: ${countryCode}</p>
+        <p>
+          <img class="unfilled" src="images/star-unfilled.png">
+          <img class="filled" src="images/star-filled.png">
+        </p>
+      </article>
+    `);
+  }
+}
+
+showAirports();
+
+// do something on click on images/stars in an airport article
+$(document).on('click', '.airport img', function () {
+  // get the airport code corresponding to which img I've clicked
+  let code = $(this).parents('.airport').find('.code').text();
+  // if favorites does not include the code then add it
+  if (!store.favorites.includes(code)) {
+    store.favorites.push(code);
+  }
+  // otherwise the code is already in favorites so remove it
+  else {
+    store.favorites = store.favorites.filter(x => x !== code);
+  }
+  store.save();
+  // show the airports again (with updated favorites)
+  showAirports();
+});
